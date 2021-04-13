41621dad4b20b57714c8ebe0be87e296890c3d7d
Hong Le <vuanhde@yahoo.de>
2021-04-13 15:07:20 +0200

    16d.update test ohne csrf-token

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -61,16 +61,19 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
 
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
-        $token = new CsrfToken('authenticate', $credentials['csrf_token']);
-        if (!$this->csrfTokenManager->isTokenValid($token)) {
-            throw new InvalidCsrfTokenException();
-        }
+        // $token = new CsrfToken('authenticate', $credentials['csrf_token']);
+        // if (!$this->csrfTokenManager->isTokenValid($token)) {
+        //     throw new InvalidCsrfTokenException();
+        // }
 
         $user = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $credentials['email']]);
 
         if (!$user) {
             // fail authentication with a custom error
             throw new CustomUserMessageAuthenticationException('Email could not be found.');
+        }
+        else {//hong
+            echo "user ist indentifiziert";
         }
 
         return $user;
